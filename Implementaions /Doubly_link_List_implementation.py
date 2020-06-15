f50efd681f4278430f11930f9706481ff9d3233a
RatnamDubey <dubey.ratnam@gmail.com>
2020-06-15 07:27:58 -0500

    All versions Implemented in the Doubly linked list

--- Implementaions /Doubly_link_List_implementation.py
+++ Implementaions /Doubly_link_List_implementation.py
@@ -22,21 +22,22 @@ class DoublyLinkList():
 
     ## need to add get length function 
 
-
-
-
-
-
+    def getlength(self):
+        count = 0 
+        temp_point = self.head
+        while temp_point.next != None:
+            temp_point = temp_point.next
+            count = count + 1 
+        return(count)
+        
     def insert(self, val):
         new_node_val = Node(val)  ## a 1
-
         ### Check if Node is Null 
         ## if null insert on the first position else traverse till end then insert 
 
         if (self.head is None) & (self.tail is None):
             self.head = new_node_val
             self.tail = new_node_val
-
         else:
             temp_point = self.head
             while temp_point.next != None:
@@ -45,32 +46,42 @@ class DoublyLinkList():
             new_node_val.prev = temp_point
             self.tail = new_node_val
 
-
-
     def append(self,val, position) :
+        cnt = self.getlength()
+        if position > cnt:
+            print("Error Invald position")
+        else:
+            new_val = Node(val)
+            if self.head is None:
+                self.head = new_val
+                self.tail = new_val
 
-        new_val = Node(val)
-        if self.head is None:
-            self.head = new_val
-            self.tail = new_val
+            else:
+                temp_position = 0 
+                temp_point = self.head
+                while temp_position != position:
+                    temp_position = temp_position+1
+                    temp_point = temp_point.next
+                
+                new_val.next = temp_point.next
+                temp_point.next.prev = new_val
+                temp_point.next = new_val
+                new_val.prev = temp_point
 
+
+    def Delete(self,position):
+        cnt = self.getlength()
+        if position > cnt:
+            print("Error Invald position")
         else:
             temp_position = 0 
             temp_point = self.head
             while temp_position != position:
                 temp_position = temp_position+1
                 temp_point = temp_point.next
-            new_val.next = temp_point.next
-            temp_point.prev = new_val
-            temp_point.next = new_val
-            new_val.prev = temp_point
-
-
-
-
-
-        
-
+            
+            temp_point.next = temp_point.next.next
+            temp_point.next.prev = temp_point
 
     # Added view Linked List content 
     def view_list_frnt(self):
@@ -89,7 +100,7 @@ class DoublyLinkList():
         return(a)
 
 
-
+# Linked List will perform all the operations that are mentioned above in the versions !! 
 
 s = DoublyLinkList()
 s.insert(10)
@@ -97,6 +108,7 @@ s.insert(11)
 s.insert(12)
 s.append(25,1)
 s.insert(13)
-
-print(s.view_list_frnt())
-print(s.view_list_back())
+s.append(5,2)
+s.Delete(0)
+print(s.view_list_frnt()) # traverse from head to tail 
+print(s.view_list_back()) # traverse from tail to head
