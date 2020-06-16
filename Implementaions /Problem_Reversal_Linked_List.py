4d01ca855a896354a9abdc84935a4e89efe773a4
RatnamDubey <dubey.ratnam@gmail.com>
2020-06-15 21:20:27 -0500

    make changes in the structure to reduce complexity

---
+++ Implementaions /Problem_Reversal_Linked_List.py
@@ -0,0 +1,10 @@
+# Linked List Reversal 
+
+## Problem
+##Write a function to reverse a Linked List in place. The function will take in the head of the list as input and return the new head of the list.
+##You are given the example Linked List Node class:
+
+
+# some_file.py
+
+import 
