58494e1fe2debe0d3a7f08c97b23bf0cdcd53075
RatnamDubey <dubey.ratnam@gmail.com>
2020-06-21 09:29:15 -0400

    Updated  the code

--- Implementaions /Implementation_Binary_Tree.py
+++ Implementaions /Implementation_Binary_Tree.py
@@ -7,48 +7,44 @@
 
 
 
+class Node():
+     def __init__(self,val):
+         self.data = val
+         self.lchild = None
+         self.rchild = None
 
-class child_node():
-
-    def __init__(self,val):
-        self.root  = val 
-        self.left = None 
-        self.right = None 
-
-    
-    
-class tree():
+class Binarytree():
 
     def __init__(self):
-        self.head = None
-        self.left = None
-        self.right = None 
-    
+        self.head = None 
+       
+    def insert(self, val):
+        new_node_val = Node(val) 
+        traverse_point = self.head
+       
+        if self.head is None:
+            self.head = new_node_val
 
-    def insert_tree(self,value):
-        if self.head is None :
-            self.head = value 
-        
-        while 1:
-            if self.head is not None:
-
-                if self.head < value:
-                    if self.right is not None:
-                        self.right = self.right.right
-                    else:
-                        self.right = child_node(value)
-                        break
+        else:
+             while traverse_point is not None:
+                print(traverse_point)
+                if val < traverse_point.data:
+                     print(val)
+                     traverse_point  = traverse_point.lchild 
+                     
+                else :
+                     traverse_point  =  traverse_point.rchild
+                print(traverse_point)    
 
                 
-        
+             
+             traverse_point = new_node_val
+             print(traverse_point.data) #
 
 
 
+b = Binarytree()
+b.insert(10)
+b.insert(9)
+print(b.head.lchild)
 
-
-
-
-tr = tree()
-tr.insert_tree(10)
-tr.insert_tree(11)
-tr.insert_tree(9)
