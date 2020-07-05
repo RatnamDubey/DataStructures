08e637db8cae6c8d84b73e87b6aa363a6a1d095c
RatnamDubey <dubey.ratnam@gmail.com>
2020-07-05 09:59:21 -0400

    Updated the heap

--- Implementaions /Implementation_Heap.py
+++ Implementaions /Implementation_Heap.py
@@ -3,54 +3,112 @@
 class maxheap():
 
     def __init__(self):
-        self.heap_list = []
+        self.heap = []
 
-    def parent_find(self,data):
-            if data %2 == 0:
-                value = data//2-1
-            else:
-                value = data//2
-            return(value)
 
-    def insert_heap(self,val):
-        if len(self.heap_list) ==  0:
-            self.heap_list.append(val)
-            return self.heap_list
+    #### ALgo 
+    # Create a max heap 
+    # If Heap is empty 
+    ####
+
+    def create_maxheap(self,value):
+
+        if len(self.heap) == 0 :
+            self.heap.append(value)
 
         else:
-            self.heap_list.append(val)
-            temp = self.heap_list[-1]
-            heap_len = len(self.heap_list)-1
-            parent = self.parent_find(heap_len)
+            self.heap.append(value)
+        #     ## Compare with parent 
+            index = len(self.heap) - 1 
+            print("element inserted is" , value)
+            print("index is", len(self.heap) -1 )
 
-            while parent >= 0:                
-                if self.heap_list[heap_len] > self.heap_list[parent]:
-                    temp = self.heap_list[heap_len]
-                    self.heap_list[heap_len] = self.heap_list[parent]
-                    self.heap_list[parent] = temp
-                    heap_len = parent
-                    parent = self.parent_find(heap_len)
+            ### parent 
+            if index % 2 == 0 :
+                parent = index //2 - 1
+            else:
+                parent = (index // 2) 
+
+            while parent >= 0:
+
+                if self.heap[index] > self.heap[parent]:
+                    self.heap[index] , self.heap[parent] = self.heap[parent] , self.heap[index]
+                    index = parent 
+                    if index % 2 == 0 :
+                        parent = index //2 - 1
+                    else:
+                        parent = (index // 2)  
+                    
                 else:
                     break
+
+
+    
+        print(self.heap)
+
+
+    def delete_heap(self,last_index):
+
+        temp = self.heap[0]
+        self.heap[0] = self.heap[last_index]
         
-        self.heap_list[heap_len] = temp
-        print(self.heap_list)
+        k = 0 
+        left_child = 2*k +1
+        right_child = 2*k + 2 
 
-                     
+        while left_child <= last_index-1: 
+
+                if self.heap[left_child] > self.heap[right_child]:
+                    if self.heap[left_child] > self.heap[k]:
+                        self.heap[k],self.heap[left_child] = self.heap[left_child],self.heap[k]
+                    
+                    k = left_child
+                    left_child = 2*k + 1 
+                    right_child = 2*k + 2 
 
 
+
+                elif self.heap[left_child] < self.heap[right_child]:
+                    if self.heap[right_child] > self.heap[k]:
+                        self.heap[k],self.heap[right_child] = self.heap[right_child],self.heap[k]
+                    k = right_child
+                    left_child = 2*k + 1
+                    right_child = 2*k + 2
+
+
+                else:
+                    break
+        self.heap[last_index] = temp
+        return self.heap
         
 
 
-mh = maxheap()
-mh.insert_heap(9)
-mh.insert_heap(4)
-mh.insert_heap(7)
-mh.insert_heap(1)
-mh.insert_heap(-2)
-mh.insert_heap(6)
-mh.insert_heap(5)
-mh.insert_heap(8)
-mh.insert_heap(80)
+
+
+
+# print('current hrap is :',c.heap)
+
+
+def main():
+    c = maxheap()
+    c.create_maxheap(5)
+    c.create_maxheap(20)
+    c.create_maxheap(25)
+    c.create_maxheap(6)
+    c.create_maxheap(30)
+    c.create_maxheap(35)
+    c.create_maxheap(40)
+    for i in range(len(c.heap)-1,-1,-1):
+        print("deleted at index"+str(i),c.delete_heap(i))
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
 
 
